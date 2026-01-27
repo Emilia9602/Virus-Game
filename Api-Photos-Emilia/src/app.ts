f65355dbbef71bbd80f3d9fc8f5ce55535a85141
Emilia Andreasson <emiliaandreasson96@gmail.com>
2026-01-27 14:10:48 +0100

    Created folder Api_Photos_Emilia and put all files inside that

---
+++ Api-Photos-Emilia/src/app.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import { rootRouter } from "./routes/root.router.ts";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Use dem routes
+app.use(rootRouter);
+
+/**
+ * Catch-all route 🛟
+ */
+app.use((req, res) => {
+	res.status(404).send({ status: "error", message: `Cannot ${req.method} ${req.path}` });
+});
+
+export default app;
