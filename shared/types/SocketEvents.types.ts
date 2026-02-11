6768e8c53c860a255568917999e076fd44f73a3a
Emilia Andreasson <emiliaandreasson96@gmail.com>
2026-02-11 12:35:08 +0100

    Added layout in socket controller

--- shared/types/SocketEvents.types.ts
+++ shared/types/SocketEvents.types.ts
@@ -1,9 +1,26 @@
+import type { Player, Room } from "../../backend/generated/prisma/client"
 export {}
 
 // Events emitted by the server to the client
 export interface ServerToClientEvents {
+
 }
 
 // Events emitted by the client to the server
 export interface ClientToServerEvents {
+
+    playerJoinRequest: (
+        username: string,
+        roomId: string,
+        callback: (response: PlayerJoinRequest) => void
+    ) => void;
+}
+
+export interface RoomWithPlayers extends Room {
+    players: Player[];
+}
+
+export interface PlayerJoinRequest {
+    success: boolean;
+    room: RoomWithPlayers | null;
 }
