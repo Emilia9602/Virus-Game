f5e66ef448db80ef00d7da530403bafdd0b915b8
Janisa <janisasmobil@icloud.com>
2026-02-11 14:57:57 +0100

    Update environment configuration, add dependencies, and implement first page layout

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import type { ClientToServerEvents, ServerToClientEvents } from "@shared/types/SocketEvents.types.ts";
 import { io, Socket } from "socket.io-client";
 import "./assets/scss/style.scss";
-
+import { createFirstPage } from "./firstPage";
 const SOCKET_HOST = import.meta.env.VITE_SOCKET_HOST;
 console.log("🙇 Connecting to Socket.IO Server at:", SOCKET_HOST);
 
@@ -12,38 +12,41 @@ const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOS
 /**
  * DOM References
  */
-
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 /**
  * Variables
  */
 
-
-/**
- * Socket Event Listeners
- */
-
-// Listen for when a connection is established
-socket.on("connect", () => {
-	console.log("💥 Connected to server", socket.io.opts.hostname + ":" + socket.io.opts.port);
-	console.log("🔗 Socket ID:", socket.id);
-});
-
-// Listen for when the server gets tired of us
-socket.on("disconnect", () => {
-	console.log("🥺 Got disconnected from server", socket.io.opts.hostname + ":" + socket.io.opts.port);
-});
-
-// Listen for when we're reconnected (either due to ours or the servers fault)
-socket.io.on("reconnect", () => {
-	console.log("🥰 Reconnected to server:", socket.io.opts.hostname + ":" + socket.io.opts.port);
-});
-
-
 /**
  * Functions
  */
+const handleStartGame =(nickname: string) => {
+	console.log("Player joined with nickname:", nickname);
+};
 
+const renderLobby = () => {
+	app.innerHTML = "";
+
+	app.appendChild(createFirstPage(handleStartGame));
+
+};
+
+//Listen for when a connection is established
+renderLobby();
+socket.on("connect", () => {
+	console.log("💥Connected to server", socket.io.opts.hostname + ":" + socket.io.opts.port);
+	console.log("Socket ID:", socket.id);
+});
+
+socket.on("disconnect", () => {
+	console.log("🥺Got disconnected from server", socket.io.opts.hostname + ":" + socket.io.opts.port);
+
+});
+
+socket.io.on("reconnect", () => {
+	console.log ("🥰 Reconnected to server:", socket.io.opts.hostname + ":" + socket.io.opts.port);
+});
 
 /**
  * DOM Event Listeners
